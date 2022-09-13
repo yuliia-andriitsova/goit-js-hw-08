25bd17d7f2a0c7a05688bc1aaecfd2927bf8c6c8
yuliia-andriitsova <andriitsovayulia@gmail.com>
2022-09-13 14:09:55 +0000

    Deploying to gh-pages from @ yuliia-andriitsova/goit-js-hw-08@c22ac697b38cf5143499379edf9b37c7461dc101 🚀

---
+++ 03-feedback.7cb836ff.js
@@ -0,0 +1,2 @@
+!function(){var e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){})),e.textarea.addEventListener("input",(function(e){var t=e.target.value;localStorage.setItem("feedback-form-state",t)}))}();
+//# sourceMappingURL=03-feedback.7cb836ff.js.map
---
+++ 03-feedback.fabf33ef.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){})),e.textarea.addEventListener("input",(function(e){const t=e.target.value;localStorage.setItem("feedback-form-state",t)}));
+//# sourceMappingURL=03-feedback.fabf33ef.js.map
